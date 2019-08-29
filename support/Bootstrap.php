6cef0dcbd1294ee9388e4219d6c0ee3d310f29a5
Andrzej Heczko <heczko@logio.cz>
2019-08-29 13:22:24 +0200

    v3 support

---
+++ support/Bootstrap.php
@@ -0,0 +1,42 @@
+<?php
+namespace BigBIT\SmartDI\Support\slim;
+
+use Psr\Container\ContainerInterface;
+use Slim\DefaultServicesProvider;
+use Slim\Collection;
+
+/**
+ * Class Bootstrap
+ * @package BigBIT\SmartDI\Support\slim
+ */
+class Bootstrap extends \BigBIT\DIBootstrap\Bootstrap {
+    public static $defaultSettings = [
+        'httpVersion' => '1.1',
+        'responseChunkSize' => 4096,
+        'outputBuffering' => 'append',
+        'determineRouteBeforeAppMiddleware' => false,
+        'displayErrorDetails' => false,
+        'addContentLengthHeader' => true,
+        'routerCacheFile' => false,
+    ];
+
+    /**
+     * @param ContainerInterface $container
+     * @param array $bindings
+     */
+    final protected static function bootContainer(ContainerInterface $container, array $bindings) {
+        parent::bootContainer($container, $bindings);
+
+        $userSettings = &$bindings['settings'];
+
+        $bindings['settings'] = function() use ($userSettings) {
+            return new Collection(array_merge(static::$defaultSettings, $userSettings));
+        };
+
+        $serviceProvider = new DefaultServicesProvider();
+
+        $serviceProvider->register(static::$container);
+
+        $container[DefaultServicesProvider::class] = $serviceProvider;
+    }
+}
